98aa20af24df572c7f3cdbfaaf623476662d673d
Evelyn <lhnguye1@ualberta.ca>
2024-05-06 08:56:59 -0600

    feat: initialize model for shop

---
+++ src/models/shop.model.js
@@ -0,0 +1,46 @@
+'use strict'
+
+//!dmbg
+const { model, Shema, types } = require('mongoose');
+
+const DOCUMENT_NAME = 'Shop';
+const COLLECTION_NAME = 'Shops';
+
+// Declare the Schema of the Mongo model
+var shopSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+        index:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    status:{
+        type:String,
+        enum:['active','inactive'],
+        default:'inactive',
+    },
+    verify: {
+        // register successfully or not
+        type: Schema.Types.Boolean,
+        default: false,
+    },
+    roles: {
+        type: Array,
+        default: [],
+    }
+}, {
+    timestamps: true,
+    collection: COLLECTION_NAME
+});
+
+//Export the model
+module.exports = model(DOCUMENT_NAME, shopSchema);
