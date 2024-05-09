2fea2f2945f8324675ac36809e83f282ad893f56
Evelyn <lhnguye1@ualberta.ca>
2024-05-09 11:24:42 -0600

    feat: implement response handling

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -2,9 +2,17 @@
 
 const AccessService = require("../services/access.service");
 
+const { OK, CREATED } = require('../core/success.response');
+
 class AccessController {
+
     signUp = async (req, res, next) => {
-        return res.status(201).json(await AccessService.signUp(req.body));
+        new CREATED (
+            {
+                message: 'User has been created',
+                metadata: await AccessService.signUp(req.body)
+            }
+        ).send(res);
     }
 }
 
--- src/core/success.response.js
+++ src/core/success.response.js
@@ -1,8 +1,50 @@
 'use strict'
 
+
+const STATUS_CODE = {
+    OK: 200,
+    CREATED: 201,
+}
+
+const ReasonStatusCode = {
+    OK: 'Success',
+    CREATED: 'Created'
+}
+
 class SuccessResponse {
-    constructor(data, statusCode) {
-        this.data = data;
+    constructor(message, statusCode = STATUS_CODE.OK, reasonStatusCode = ReasonStatusCode.OK, metadata = {}) {
+        // default is success
+        // if statuscode and reason status code is not provided, assume it is success ( 200 )
+        this.message = !message ? reasonStatusCode : message;
         this.statusCode = statusCode;
+        this.reasonStatusCode = reasonStatusCode;
+        this.metadata = metadata;
     }
+
+    send(res, header = {} ) {
+        return res.status(this.statusCode).json({
+            message: this.message,
+            reasonStatusCode: this.reasonStatusCode,
+            metadata: this.metadata
+        });
+    }
+}
+
+class OK extends SuccessResponse {
+    constructor(message , metadata = {}) {
+        super(message, metadata)
+    }
+}
+
+class CREATED extends SuccessResponse {
+    // pass in status code and reason status code to differentiate between success and created
+    constructor(message, statuscode = STATUS_CODE.CREATED, reasonStatusCode = ReasonStatusCode.CREATED, metadata = {}) {
+        super(message, statuscode, reasonStatusCode, metadata)
+    }
+}
+
+module.exports = {
+    SuccessResponse,
+    OK,
+    CREATED
 }
